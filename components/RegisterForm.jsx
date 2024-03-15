eef38baf2912236f8e162b3d53dd225f85610dfa
Micka <mickael.murmann@gmail.com>
2024-03-15 11:26:04 +0100

    optimization of registerForm

--- components/RegisterForm.jsx
+++ components/RegisterForm.jsx
@@ -1,11 +1,10 @@
 "use client";
 
-import Link from "next/link"
+import Link from "next/link";
 import { useState } from "react";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 
-const RegisterForm = () => {
-
+export default function RegisterForm() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -16,110 +15,98 @@ const RegisterForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
-    if(!name || !email || !password) {
-        setError("All fields are required");
-        return;
+
+    if (!name || !email || !password) {
+      setError("All fields are necessary.");
+      return;
     }
 
     if(password !== passwordRepeat) {
-        setError("Password do not match");
+        setError("Passwords do not match");
         return;
     }
 
     try {
-        const res = await fetch('api/checkUserExists/', {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                email, 
-            }),
-        })
+      const resUserExists = await fetch("api/userExists", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email }),
+      });
 
-        if(res.ok) {
-            setError("User already exists");
-            return;
-        } 
+      const { user } = await resUserExists.json();
 
-        setError("");
+      if (user) {
+        setError("User already exists.");
+        return;
+      }
+
+      const res = await fetch("api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+        }),
+      });
+
+      if (res.ok) {
         const form = e.target;
-        form.reset(); 
-
-    } catch(error) {
-        console.log("Error during registration :", error)
-    }
-    
-    try {
-        const res = await fetch('api/register/', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                name, 
-                email, 
-                password,
-            }),
-        });
-        
-        if(res.ok) {
-            // const form = e.target;
-            // form.reset(); 
-            router.push('/');
-        } else {
-            console.log("User registration failed")
-        }
+        form.reset();
+        router.push("/");
+      } else {
+        console.log("User registration failed.");
+      }
     } catch (error) {
-        console.log("Error during registration :", error)
+      console.log("Error during registration: ", error);
     }
-  }
+  };
 
   return (
     <div className="grid place-items-center h-screen">
-        <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
-            <h1 className="text-xl font-bold my-4">Register</h1>
-            <form 
-                onSubmit={handleSubmit}
-                className="flex flex-col gap-3"
-            >
-                <input 
-                    onChange={ e => setName(e.target.value) } 
-                    type="text" 
-                    placeholder="Full Name" 
-                    />
-                <input 
-                    onChange={ e => setEmail(e.target.value) } 
-                    type="text" 
-                    placeholder="Email" 
-                    />
-                <input 
-                    onChange={ e => setPassword(e.target.value) } 
-                    type="password" 
-                    placeholder="Password" 
-                />
-                <input 
-                    onChange={ e => setPasswordRepeat(e.target.value) } 
-                    type="password" 
-                    placeholder="Repeat Password" 
-                />
-                <button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2">
-                    Register
-                </button>
+      <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
+        <h1 className="text-xl font-bold my-4">Register</h1>
 
-                { error && (
-                    <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
-                        {error}
-                    </div>
-                )}
-                <Link className="text-sm mt-3 text-right" href={'/'}>
-                    Already have an account ? <span className="underline">Login</span>
-                </Link>
-            </form>
-        </div>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+          <input
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            placeholder="Full Name"
+          />
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            placeholder="Email"
+          />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Password"
+          />
+          <input
+            onChange={(e) => setPasswordRepeat(e.target.value)}
+            type="password"
+            placeholder="Repeat Password"
+          />
+          <button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2">
+            Register
+          </button>
+
+          {error && (
+            <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+              {error}
+            </div>
+          )}
+
+          <Link className="text-sm mt-3 text-right" href={"/"}>
+            Already have an account? <span className="underline">Login</span>
+          </Link>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
-
-export default RegisterForm
