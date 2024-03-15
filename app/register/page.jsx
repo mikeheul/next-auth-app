9ee006d3e77a5cd4a30e9e7e57625a462f015cbb
Micka <mickael.murmann@gmail.com>
2024-03-15 15:59:59 +0100

    login success

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,6 +1,14 @@
-const { default: RegisterForm } = require("@/components/RegisterForm")
+import RegisterForm from "@/components/RegisterForm";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
-const page = () => {
+const page = async () => {
+  
+  const session = await getServerSession(authOptions);
+  
+  if(session) redirect("/dashboard");
+
   return (
     <RegisterForm />
   )
